c9ed71048e64bcb771bb495ff0196e44e9ab9d7e
Dinesh Gamage <dinesh@ecyber.com>
2020-09-22 17:34:03 +0530

    updated

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
 
 const Welcome: React.FunctionComponent<{}> = (props) => {
-    return<div className="welcome-block">
-        <h2> EXPERIENCE DASHBOARD - SHOWCASE </h2>
-    </div>
+    return (<div className="section">
+
+        <div className="showcase-section bgWhite welcome-block"  >
+            <h1>Experience Portal showcase</h1>
+            <br />
+            <p>
+                Welcome to the Lucy Experience Portal showcase! <br/><br/>
+
+                The experience portal part of Lucy allows you to deliver rich analytics and user journeys to your customers. <br/>
+
+                This site contains example usage of components that are available in the experience portal. <br/>
+            </p>
+            <br /><br />
+        </div>
+    </div>)
 }
 
 export default Welcome
